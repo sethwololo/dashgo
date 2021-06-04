bf49acd719d9ca849cd0a3f5cb4cc31f41089666
Matheus Henrique <sethwololo@gmail.com>
2021-06-04 16:38:17 -0300

    Listagem de usuários

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import {
   Box,
   Flex,
@@ -12,10 +13,12 @@ import {
   Tbody,
   Td,
   Text,
-  useBreakpointValue
+  useBreakpointValue,
+  Center,
+  Spinner
 } from "@chakra-ui/react";
+import { useQuery } from 'react-query';
 import { RiAddLine, RiPencilLine } from 'react-icons/ri';
-import Link from 'next/link';
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
@@ -23,16 +26,16 @@ import { Sidebar } from "../../components/Sidebar";
 import { useEffect } from "react";
 
 export default function UsersList() {
+  const { data, isLoading, error } = useQuery('users', async () => {
+    const response = await fetch('http://localhost:3000/api/users');
+    const data = await response.json();
+    return data;
+  });
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true
   });
-
-  useEffect(() => {
-    fetch('http://localhost:3000/api/users')
-      .then(response => response.json())
-      .then(data => console.log());
-  }, []);
 
   return (
     <Box>
@@ -57,45 +60,57 @@ export default function UsersList() {
             </Link>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
-            <Thead>
-              <Tr>
-                <Th px={["2", "4", "6"]} color="gray.300" width="8">
-                  <Checkbox colorScheme="pink" />
-                </Th>
-                <Th>Usuário</Th>
-                {isWideVersion && <Th>Data de cadastro</Th>}
-                <Th width={["2", "8"]}></Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              <Tr>
-                <Td px={["2", "4", "6"]}>
-                  <Checkbox colorScheme="pink" />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight="bold" fontSize={["xs", "md"]}>Matheus Henrique Moura Bezerra</Text>
-                    <Text fontSize={["xs", "sm"]} color="gray.300">sethwololo@gmail.com</Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>17 de maio de 2021</Td>}
-                <Td px={["2", "8"]}>
-                  <Button
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={isWideVersion ? <Icon as={RiPencilLine} fontSize="16" /> : undefined}
-                  >
-                    {isWideVersion ? 'Editar' : <Icon as={RiPencilLine} fontSize="16" />}
-                  </Button>
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
+          {isLoading ? (
+            <Center>
+              <Spinner />
+            </Center>
+          ) : error ? (
+            <Center>
+              <Text>Falha ao obter dados dos usuários</Text>
+            </Center>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={["2", "4", "6"]} color="gray.300" width="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Usuário</Th>
+                    {isWideVersion && <Th>Data de cadastro</Th>}
+                    <Th width={["2", "8"]}></Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  <Tr>
+                    <Td px={["2", "4", "6"]}>
+                      <Checkbox colorScheme="pink" />
+                    </Td>
+                    <Td>
+                      <Box>
+                        <Text fontWeight="bold" fontSize={["xs", "md"]}>Matheus Henrique Moura Bezerra</Text>
+                        <Text fontSize={["xs", "sm"]} color="gray.300">sethwololo@gmail.com</Text>
+                      </Box>
+                    </Td>
+                    {isWideVersion && <Td>17 de maio de 2021</Td>}
+                    <Td px={["2", "8"]}>
+                      <Button
+                        as="a"
+                        size="sm"
+                        fontSize="sm"
+                        colorScheme="purple"
+                        leftIcon={isWideVersion ? <Icon as={RiPencilLine} fontSize="16" /> : undefined}
+                      >
+                        {isWideVersion ? 'Editar' : <Icon as={RiPencilLine} fontSize="16" />}
+                      </Button>
+                    </Td>
+                  </Tr>
+                </Tbody>
+              </Table>
 
-          <Pagination />
+              <Pagination />
+            </>
+          )}
         </Box>
       </Flex>
     </Box>
